b12453cf5a044dbcf89ef1c7d9a54a741fb5ea44
Fatih Kazancı <33206545+fatihkazanci@users.noreply.github.com>
2022-04-09 14:53:41 +0300

    Add files via upload

---
+++ ExcelReaderWriterLibrary/ExcelReaderWriterLibrary/ExampleExcelModel.cs
@@ -0,0 +1,16 @@
+﻿using ExcelStreamerLibrary.Attributes;
+using ExcelStreamerLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExcelReaderWriterLibrary
+{
+    public class ExampleExcelModel : ExcelStreamerObject
+    {
+        [ExcelStreamerSheetName("Yapılacaklar Listesi")]
+        public List<ExampleExcelWorkSheetModel> ToDoList { get; set; }
+    }
+}
---
+++ ExcelReaderWriterLibrary/ExcelReaderWriterLibrary/ExampleExcelWorkSheetModel.cs
@@ -0,0 +1,18 @@
+﻿using ExcelStreamerLibrary.Attributes;
+using ExcelStreamerLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExcelReaderWriterLibrary
+{
+    public class ExampleExcelWorkSheetModel: ExcelStreamerWorkSheetObject
+    {
+        [ExcelStreamerColumnLetter("a")]
+        public string Name { get; set; }
+        [ExcelStreamerColumnLetter("b")]
+        public string Surname { get; set; }
+    }
+}
