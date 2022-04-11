9b04f1e9c5dd003b380785fab99a68d6893f17e5
Fatih Kazancı <33206545+fatihkazanci@users.noreply.github.com>
2022-04-11 16:57:17 +0300

    Add files via upload

---
+++ ExcelReaderWriterLibrary/ExcelStreamerLibrary/Attributes/ExcelStreamerWorkSheetName.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExcelStreamerLibrary.Attributes
+{
+    public class ExcelStreamerWorkSheetName : Attribute
+    {
+        public string SheetName { get; set; }
+        public ExcelStreamerWorkSheetName(string sheetName)
+        {
+            this.SheetName = sheetName;
+        }
+    }
+}
